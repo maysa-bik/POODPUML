163c5365e0bc4cec331e623cd1a0bab35f16238b
maysa-bik <maysa.bik@laplateforme.com>
2024-05-28 11:01:02 +0200

    je crée un fichier main.cpp

--- main.cpp
+++ main.cpp
@@ -3,18 +3,18 @@
 
 int main()
 {
-    // std::cout << "Hello" << std::endl;
-    // sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
-    // while (window.isOpen())
-    // {
-    //     sf::Event event;
-    //     while (window.pollEvent(event))
-    //     {
-    //         if (event.type == sf::Event::Closed)
-    //             window.close();
-    //     }
-    //     window.clear();
-    //     window.display();
-    // }
+    std::cout << "Hello" << std::endl;
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear();
+        window.display();
+    }
     return 0;
 }
